65c168fd70cf4e59cec8679b39b398a0b1c32f8f
frh <frh.shaalan@gmail.com>
2022-12-08 00:07:52 +0300

    Initial Commit

---
+++ bookingpage/bookingpageApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookingpageApp.swift
+//  bookingpage
+//
+//  Created by frh alshaalan on 14/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct bookingpageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
